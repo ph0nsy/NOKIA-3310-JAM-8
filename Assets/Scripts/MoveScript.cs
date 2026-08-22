90d1e81c1b22930ae3f04fda4bb07ccc056f80ef
ph0nsy-utad <alonso.moreno@live.u-tad.com>
2026-08-22 20:06:02 +0200

    Miscellaneous

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class MoveScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  [Range(1f, 50f)]
+  public float Speed = 10; // m/s
+  [SerializeField]
+  [Range(0,1)]
+  private Vector2 Direction; // espacio de coordenadas
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  private Transform tfm;
+  
+  private void Awake()
+  {
+    Direction = new Vector2(0.5f, 0.5f);
+  }
+
+  void Start()
+  {
+    tfm = GetComponent<Transform>();
+  }
+
+  private void FixedUpdate()
+  {
+    // frame = 1/120 s 
+    // Time.deltaTime en segs > Tiempo en fixedupdate(x) - Tiempo en fixedupdate(x-1)
+    Vector3 pos = tfm.position; // p0
+    pos.x += Speed * Direction.x * Time.deltaTime; // p1.x
+    pos.y += Speed * Direction.y * Time.deltaTime; // p1.y
+    tfm.position = pos;
+
+    // 10 m/s = 10/x m/x(s)
+
+    // 10 m/s = x m/h
+  }
 }
