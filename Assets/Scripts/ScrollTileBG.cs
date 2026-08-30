680ae2c0454e48ce1dd2bab783c20dd9fa726a83
ViskoCode <marianodlt99@gmail.com>
2026-08-30 20:16:38 +0200

    FINAL

--- Assets/Scripts/ScrollTileBG.cs
+++ Assets/Scripts/ScrollTileBG.cs
@@ -12,7 +12,7 @@ public class ScrollTileBG : MonoBehaviour
     private Image img;
     private Vector2 cummulativeSpeed;
 
-    private const float pixelPercent = 1f/84f;
+    private const float pixelPercent = 1f/84;
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +20,12 @@ public class ScrollTileBG : MonoBehaviour
         img = GetComponent<Image>();
         speed = speed / (pixelPercent * 100f); // Speed to our expected pixel size
         cummulativeSpeed = new Vector2(0f, 0f);
+        // img.material.mainTextureOffset += new Vector2(/*-pixelPercent*(8/39 + 7/134)*/ 0,0);
+
     }
 
     // Update is called once per frame
-    void Update()
+    void Update()   
     {
         cummulativeSpeed += direction.normalized * Time.deltaTime * speed;
         Vector2 resultingOffset = new Vector2(0f, 0f);
@@ -39,7 +41,10 @@ public class ScrollTileBG : MonoBehaviour
             resultingOffset.y = pixelPercent;
             cummulativeSpeed.y = 0;
         }
-
+        Debug.Log(img.material.mainTextureOffset.x);
         img.material.mainTextureOffset += resultingOffset;
+        img.material.mainTextureOffset = new Vector2(
+            img.material.mainTextureOffset.x>=1?0:img.material.mainTextureOffset.x,
+           img.material.mainTextureOffset.y>=1?0:img.material.mainTextureOffset.y);
     }
 }
